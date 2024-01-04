e085f1bd897a59dd47168a6c60c15b7678e71cd2
Carlos Agamez <carlos.agamez@sigma3ds.com>
2024-01-04 13:45:35 -0500

    seccion de proyectos y about terminadas

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,15 +4,15 @@ from datetime import date
 
 class Project(models.Model):
     STATUS_CHOICES = [
-        ('en_proceso', 'En Proceso'),
-        ('terminado', 'Terminado'),
-        ('pendiente', 'Pendiente'),  # Puedes agregar más estados según tus necesidades
+        ('Desarrollando', 'Desarrollando'),
+        ('Terminado', 'Terminado'),
+        ('Pendiente', 'Pendiente'),  
     ]
 
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250)
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='en_proceso')
-    image = models.ImageField(upload_to="portfolio/images")
+    image = models.ImageField(upload_to="portfolio/", blank=True)
     url = models.URLField(blank=True)
 
     def __str__(self) -> str:
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-
+from .models import Project
 
 def index(request):
-    
-    
-    return render(request, "portfolio/index.html")
+    return render(request, "portfolio/index.html",{
+        "projects": Project.objects.all(),
+        'estados':[cat[1] for cat in Project.STATUS_CHOICES],
+    })
